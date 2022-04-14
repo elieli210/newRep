657289b9fa2b3a4eef82f1e9df6cf90b2acd03ad
Elmira <elmira.rezabeyk@gmail.com>
2022-04-14 14:37:11 +0430

    changes

---
+++ 1.js
@@ -0,0 +1,9 @@
+console.log("hello");
+//1
+// //Follow this steps:
+// Init Git in project
+// Check status using “Git status”
+// Change content of them
+// Check status using “Git status”
+// Commit
+// Check status using “Git status”
